8d20410d76ca112117315a2df0ebc76eedec823e
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-17 11:01:57 -0500

    Finished setting up the project

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Karla } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const karla = Karla({ subsets: ["latin"]})
 
 export const metadata = {
   title: "Create Next App",
@@ -18,9 +10,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${karla.variable} antialiased`}
+        /* Suppress the hydration warnings: */
+        suppressHydrationWarning
       >
         {children}
       </body>
