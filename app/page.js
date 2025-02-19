389e0dffb51cd6318ab6ed42d8af2e074e801c33
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-19 00:47:41 -0500

    Added the buttons at the bottom of the quiz and the instruction on how to start recording. i.e. the UI of the bottom part of each question

--- app/page.js
+++ app/page.js
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import Option from "./components/Option";
 import { Poppins } from "next/font/google";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid"
+import { MicrophoneIcon } from "@heroicons/react/24/outline"
+
 
 
 const poppins = Poppins({subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]})
@@ -34,28 +37,54 @@ export default function Home() {
         <div className="flex flex-col bg-[#F0F0F0] w-full h-full px-5
                         rounded-2xl justify-center items-center"
         >
-          {/* Question: */}
-          <div className="flex flex-row gap-3 pb-4">
-            <p>{questionNum}.</p>
-            <p className="text-2xl">{question}</p>
+          {/* Question & Answers */}
+          <div className="flex flex-col justify-center items-center w-full h-4/5">
+            {/* Question: */}
+            <div className="flex flex-row gap-3 pb-4">
+              <p>{questionNum}.</p>
+              <p className="text-2xl">{question}</p>
+            </div>
+
+            {/* Answer Options: */}
+            <div className="flex flex-wrap w-2/5 px-10 justify-center gap-x-5">
+              {options.map((option, index) => (
+                <div key={option} className="flex flex-row gap-3 py-2">
+                  {/* Display all the options: */}
+                  <Option 
+                    index={index}
+                    text={option}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
 
-          {/* Answer Options: */}
-          <div className="flex flex-wrap w-2/5 px-10 justify-center gap-x-5">
-            {options.map((option, index) => (
-              <div key={option} className="flex flex-row gap-3 py-2">
-                {/* Display all the options: */}
-                <Option 
-                  index={index}
-                  text={option}
+          {/* Bottom Part */}
+          <div className="flex flex-col items-center w-full">
+            {/* Buttons */}
+            <div className="flex justify-between items-center w-full px-5">
+              {/* Prev Button: */}
+              <button className="bg-black p-2 rounded-sm">
+                <ChevronLeftIcon className="size-4 stroke-gray-300 stroke-[2]"
+                                strokeLinecap="round" strokeLinejoin="round"
                 />
-              </div>
-            ))}
-          </div>
+              </button>
 
-          {/* Bottom Part" */}
-          <div className="">
-            {/* Button */}
+              {/* Speaker Button: */}
+              <button className="border-black border-[1px] rounded-lg p-2">
+                <MicrophoneIcon className="size-5 stroke-[1.35]" fill="none"/>
+              </button>
+
+              {/* Next Button: */}
+              <button className="bg-black p-2 rounded-sm">
+                <ChevronRightIcon className="size-4 stroke-gray-300 stroke-[2]"
+                                strokeLinecap="round" strokeLinejoin="round"
+                />
+              </button>
+            </div>
+
+            {/* Instruction: */}
+            <p className="text-xs italic mt-3 text-gray-500">press spacebar to start speaking</p>
           </div>
         </div>
       </div>
