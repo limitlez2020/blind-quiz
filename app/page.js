678c076b544fb2af7f1e54d047ad20788b243220
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-19 01:02:38 -0500

    Editted the text instruction below the microphone

--- app/page.js
+++ app/page.js
@@ -84,7 +84,10 @@ export default function Home() {
             </div>
 
             {/* Instruction: */}
-            <p className="text-[10px] sm:text-xs italic mt-3 text-gray-500">press spacebar to start speaking</p>
+            {/* Laptops: */}
+            <p className="hidden sm:block text-xs italic mt-3 text-gray-500">press spacebar to start speaking</p>
+            {/* Phones/Tablets: */}
+            <p className="block sm:hidden text-[10px] italic mt-3 text-gray-500">click to start speaking</p>
           </div>
         </div>
       </div>
