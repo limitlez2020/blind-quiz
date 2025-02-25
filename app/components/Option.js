07207336a3a077d9cfc3a2fb33e67b309674bff6
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-25 11:46:55 -0500

    Editted spacing/padding of the Options component

--- app/components/Option.js
+++ app/components/Option.js
@@ -39,7 +39,7 @@ export default function Option({index, text, isSelected, onSelect}) {
   return (
     <button className="w-" onClick={onSelect}>
       <div className={`${isSelected ? "border-2" : "border-[1px]"} flex flex-row gap-2
-                    border-black bg-[#DEDDDE] p-2 pr-10 rounded-sm items-center`}>
+                    border-black bg-[#DEDDDE] p-2 pr-20 rounded-sm items-center`}>
         {/* Option Letter: */}
         <div className={`${isSelected ? "bg-black text-white" : "bg-[#EBEAEC] text-black border-black border-[1px]"}
                       ${poppins.className} text-sm font-medium px-2 py-1 rounded-sm`}>
