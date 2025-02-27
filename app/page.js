04aee286106e32b3891725c1a4b9f5eb13ee741c
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-27 12:24:13 -0500

    Implemented the basic UI of the home page

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@
 import { useState } from "react";
 import NavBar from "./components/NavBar";
 import { Poppins } from "next/font/google";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid"
-import { MicrophoneIcon, SpeakerWaveIcon } from "@heroicons/react/24/outline"
+import { ArrowTurnDownRightIcon } from "@heroicons/react/24/solid"
 import Link from "next/link";
 
 
@@ -13,117 +12,16 @@ const poppins = Poppins({subsets: ["latin"], weight: ["100", "200", "300", "400"
 
 
 export default function Home() {
-  /* Keep track of the current question */
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  /* Array of question objects: */
-  const questions = [
-    {
-      question: "What is the name of the current President of the US?",
-      options: ["Donald Trump", "Barrack Obama", "Ronald Reagan", "Kamala Harris"],
-      correctAnswer: "Donald Trump",
-    },
-    {
-      question: "What is the capital of France?",
-      options: ["Paris", "London", "Berlin", "Madrid"],
-      correctAnswer: "Paris",
-    },
-    {
-      question: "Which planet is known as the Red Planet?",
-      options: ["Earth", "Mars", "Jupiter", "Venus"],
-      correctAnswer: "Mars",
-    },
-    {
-      question: "Who wrote the play 'Romeo and Juliet'?",
-      options: ["William Shakespeare", "Charles Dickens", "Mark Twain", "Jane Austen"],
-      correctAnswer: "William Shakespeare",
-    },
-    {
-      question: "What is the largest ocean on Earth?",
-      options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
-      correctAnswer: "Pacific Ocean",
-    },
-    {
-      question: "Which element has the chemical symbol 'O'?",
-      options: ["Oxygen", "Gold", "Iron", "Carbon"],
-      correctAnswer: "Oxygen",
-    },
-    {
-      question: "What is the smallest prime number?",
-      options: ["1", "2", "3", "5"],
-      correctAnswer: "2",
-    },
+  /* Array of instructions: */
+  const instructions = [
+    "Play ball with friends",
+    "Don't take everything to heart",
+    "Be kind to others",
+    "Learn to play the guitar",
+    "Eat a salad for lunch",
   ];
 
-  /* Array of selected answers by the user
-   * Create an array with the same size as the # questions and 
-   * fill it with empty strings */
-  const [selectedAnswers, setSelectedAnswers] = useState(Array(questions.length).fill(""));
-
   
-  
-  /* Move to previous question */
-  const prevQuestion = () => {
-    /* Only move to the previous question if we're not on the first question: */
-    if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1)
-    }
-  }
-
-
-  /* Move to next question */
-  const nextQuestion = () => {
-    /* Only move to next question if we're not on last question: */
-    if (currentQuestionIndex < questions.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1)
-    }
-  }
-
-
-
-  /* Submit the quiz */
-  const handleSubmit = () => {
-    /* Check if all questions have been answered: */
-    if (selectedAnswers.includes("")) {
-      /* Display the question numbers that have been unanswered: */
-      const unansweredQuestionNums = []
-      for (let i = 0; i < selectedAnswers.length; i++) {
-        if (selectedAnswers[i] === "") {
-          /* We add one to cover for the 0-indexing */
-          unansweredQuestionNums.push(i + 1)
-        }
-      }
-      
-      /* Print the questions unanswered: */
-      alert(`You still have some questions unanswered: ${unansweredQuestionNums}`)
-      return
-    }
-
-    /* Check if answers are correct: */
-    let score = 0;
-    questions.forEach((question, index) => {
-      /* Keep track of the how many gotten right */
-      if (question.correctAnswer === selectedAnswers[index]) {
-        score += 1;
-      }
-    })
-    
-    /* Display score and percentage */
-    let percentage = (score / questions.length) * 100
-    alert(`Congrats! You got ${score} out of ${questions.length} \nPercentage is: ${percentage}`)
-  }
-
-
-
-  /* Handle the choosing of options by user
-   * Keep track of the options chosen */
-  const handleOptionSelect = (option) => {
-    /* Update the selected answers array with the
-     * option chosen for the current question */
-    /* Make a copy of the selected answers */
-    const newAnswers = [...selectedAnswers];
-    newAnswers[currentQuestionIndex] = option;
-    setSelectedAnswers(newAnswers);
-  }
 
 
 
@@ -142,13 +40,20 @@ export default function Home() {
         >
           {/* Instructions */}
           <div>
-            Here are the Instructions
+            {/* Header */}
+            <p className="text-2xl">Instructions</p>
+            
+            {/* Instruction */}
+            <p> Instruction 1. {instructions[0]}</p>
+
           </div>
 
           {/* Start Quiz: */}
           <Link href={"/quizPage"}>
-            <button className="bg-black size-10">
-              Start Quiz
+            <button className="flex flex-row items-center justify-center
+                             border-black border-[1px] py-2 px-3 gap-2">
+              <p>Begin</p>
+              <ArrowTurnDownRightIcon className="size-4"/>
             </button>
           </Link>
           
