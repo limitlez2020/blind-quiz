63fbf7ebe88d8547f22dec5a132ea73dc8185ac8
fortune.akinremi <fortuneakinremi@gmail.com>
2025-02-28 01:10:37 -0500

    Updated the UI of the home page and added real instructions

--- app/page.js
+++ app/page.js
@@ -14,11 +14,11 @@ const poppins = Poppins({subsets: ["latin"], weight: ["100", "200", "300", "400"
 export default function Home() {
   /* Array of instructions: */
   const instructions = [
-    "Play ball with friends",
-    "Don't take everything to heart",
-    "Be kind to others",
-    "Learn to play the guitar",
-    "Eat a salad for lunch",
+    "'start quiz' = begin quiz",
+    "'next'       = next question",
+    "'previous'   = previous question",
+    "'option _'   = choose answer",
+    "'submit'     = submit the quiz",
   ];
 
   
@@ -34,28 +34,36 @@ export default function Home() {
 
       {/* Main Body: */}
       <div className="flex w-full h-[41rem] px-6 justify-center items-center">
-        {/* Container */}
+        {/* Gray Container */}
         <div className="flex flex-col bg-[#F0F0F0] w-full h-full px-5
                         rounded-2xl justify-center items-center"
         >
-          {/* Instructions */}
-          <div>
-            {/* Header */}
-            <p className="text-2xl">Instructions</p>
-            
-            {/* Instruction */}
-            <p> Instruction 1. {instructions[0]}</p>
+          {/* Content Container */}
+          <div className="flex flex-col items-start">
+            {/* Instructions */}
+            <div>
+              {/* Header */}
+              <p className="text-2xl">Instructions</p>
+              {/* Subheader */}
+              <p className="-mt-1 mb-1 text-xs">Say these words to control quiz</p>
+              
+              {/* Instruction */}
+              {instructions.map((instruction, index) => (
+                <div className="mb-1" key={index}>
+                  {index + 1}. {instruction}
+                </div>
+              ))}
+            </div>
 
+            {/* Start Quiz: */}
+            <Link href={"/quizPage"}>
+              <button className="flex flex-row items-center justify-center self-end
+                              border-black border-[2px] py-2 px-3 gap-2 text-sm mt-2">
+                <p>Begin</p>
+                <ArrowTurnDownRightIcon className="mt-[5px] size-3"/>
+              </button>
+            </Link>
           </div>
-
-          {/* Start Quiz: */}
-          <Link href={"/quizPage"}>
-            <button className="flex flex-row items-center justify-center
-                             border-black border-[1px] py-2 px-3 gap-2">
-              <p>Begin</p>
-              <ArrowTurnDownRightIcon className="size-4"/>
-            </button>
-          </Link>
           
         </div>
       </div>
