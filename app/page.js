4f367b4fdb720264b8d14ecbd2af7814d3e47469
fortune.akinremi <fortuneakinremi@gmail.com>
2025-03-04 12:43:51 -0500

    Added an instruction on the home page to click on the screen to hear the instructions

--- app/page.js
+++ app/page.js
@@ -136,42 +136,52 @@ export default function Home() {
       {/* Main Body: */}
       <div className="flex w-full h-[41rem] px-6 justify-center items-center">
         {/* Gray Container */}
-        <div className="flex flex-col bg-[#F0F0F0] w-full h-full px-5
-                        rounded-2xl justify-center items-center"
+        <div className="relative bg-[#F0F0F0] w-full h-full px-5
+                        rounded-2xl"
         >
-          {/* Content Container */}
-          <div className="flex flex-col items-start">
-            {/* Instructions */}
-            <div>
-              {/* Header */}
-              <p className="text-2xl">Instructions</p>
-              {/* Subheader */}
-              <p className="-mt-1 mb-4 text-xs">Say these words to control quiz</p>
-              
-              {/* Instruction */}
-              {instructions.map((instruction, index) => (
-                <div className="mb-2 flex flex-row gap-2 items-center" key={index}>
-                  {/* Number */}
-                  <div className="border-[1px] text-sm border-black py-[3px] px-[9px]">
-                    {index + 1}.
-                  </div>
-                  {/* Text */}
-                  <p>
-                    {instruction}
-                  </p>
-                </div>
-              ))}
-            </div>
+          {/* Click screen instruction: */}
+          <div className="flex absolute top-4 left-1/2 -translate-x-1/2 items-center whitespace-pre">
+            {/* <p>[</p> */}
+            <p className="italic border-b-[1px] border-black text-xs"> click screen to hear instructions </p>
+            {/* <p>]</p> */}
+          </div>
 
-            {/* Start Quiz: */}
-            <Link href={"/quizPage"}>
-              <button className="flex flex-row items-center justify-center
-                              bg-black text-white py-2 px-3 gap-2 mt-2">
-                <p className={`${poppins.className} text-sm font-mednium`}>Begin</p>
-                <ArrowTurnDownRightIcon className="size-3"/>
-              </button>
-            </Link>
-            <p className="hidden sm:block text-xs italic text-neutral-500">press spacebar to begin quiz</p>
+          {/* Outer Container: */}
+          <div className="flex h-full items-center justify-center">
+            {/* Inner Container */}
+            <div className="flex flex-col items-start">
+              {/* Instructions */}
+              <div>
+                {/* Header */}
+                <p className="text-2xl">Instructions</p>
+                {/* Subheader */}
+                <p className="-mt-1 mb-4 text-xs">Say these words to control quiz</p>
+                
+                {/* Instruction */}
+                {instructions.map((instruction, index) => (
+                  <div className="mb-2 flex flex-row gap-2 items-center" key={index}>
+                    {/* Number */}
+                    <div className="border-[1px] text-sm border-black py-[3px] px-[9px]">
+                      {index + 1}.
+                    </div>
+                    {/* Text */}
+                    <p>
+                      {instruction}
+                    </p>
+                  </div>
+                ))}
+              </div>
+
+              {/* Start Quiz: */}
+              <Link href={"/quizPage"}>
+                <button className="flex flex-row items-center justify-center
+                                bg-black text-white py-2 px-3 gap-2 mt-2">
+                  <p className={`${poppins.className} text-sm font-mednium`}>Begin</p>
+                  <ArrowTurnDownRightIcon className="size-3"/>
+                </button>
+              </Link>
+              <p className="hidden sm:block text-xs italic text-neutral-500">press spacebar to begin quiz</p>
+            </div>
           </div>
           
         </div>
